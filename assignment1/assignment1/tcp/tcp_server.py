54713677668bc42aa5e7b3e19b3aabaa7024f056
miaoxiaogu <60529909+miaoxiaogu@users.noreply.github.com>
2020-02-27 17:42:14 -0800

    Add files via upload

---
+++ assignment1/assignment1/tcp/tcp_server.py
@@ -0,0 +1,32 @@
+import socket, threading
+
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5000
+BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(10)
+
+def listen_forever(conn, addr):
+
+
+    """conn, addr = s.accept()"""
+    print(f'Connection address:{addr}')
+
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            print('No data received.')
+            break
+        print(f"received data:{data.decode()}")
+        conn.send("pong".encode())
+    
+
+while True:
+    conn,addr = s.accept()
+    sc_client = threading.Thread(target = listen_forever, args = (conn,addr))
+    sc_client.start()
+
+
