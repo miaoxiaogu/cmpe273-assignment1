bec4eb55bd68ba290d89b788066b59aab1d6b364
miaoxiaogu <60529909+miaoxiaogu@users.noreply.github.com>
2020-03-03 22:58:51 -0800

    Update tcp_client.py

--- assignment1/assignment1/tcp/tcp_client.py
+++ assignment1/assignment1/tcp/tcp_client.py
@@ -1,23 +1,31 @@
-import socket
-
-
+import socket,sys,time
+#
+#
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
 MESSAGE = "ping"
 
-def send(id=0):
+def send(id):
+    delayInSec = int(sys.argv[2])
+    numOfPingMessage = int(sys.argv[3])
+    num = 0
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(f"{id}:{MESSAGE}".encode())
-    data = s.recv(BUFFER_SIZE)
+    while num < numOfPingMessage:
+        s.send(f"{id}:{MESSAGE}".encode())
+        print(f"Sending data:{MESSAGE}")
+        data = s.recv(BUFFER_SIZE)
+        print("Received data:", data.decode())
+        num += 1
+        time.sleep(delayInSec)
     s.close()
-    print("received data:", data.decode())
 
-
+#
+#
 def get_client_id():
-    id = input("Enter client id:")
+    id = sys.argv[1]
     return id
 
-
-send(get_client_id())
+if __name__== "__main__":
+    send(get_client_id())
